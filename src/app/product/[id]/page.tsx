4ffb12eb95bb0114cba4229c82e0f1c0b863e289
ahamadoubah <bah.t@northeastern.edu>
2025-02-24 21:47:59 -0500

    ......

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -9,8 +9,9 @@ export async function generateStaticParams() {
   }));
 }
 
-export default function ProductDetails({ params }: { params: { id: string } }) {
-  const product = products.find(p => p.id === params.id);
+export default async function ProductDetails({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params
+  const product = products.find(p => p.id === id);
   
   if (!product) {
     notFound();
