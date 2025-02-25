db27d75f8f7439286551e0473ed40a34ccb93c63
ahamadoubah <bah.t@northeastern.edu>
2025-02-24 21:41:54 -0500

    ...

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,7 +1,13 @@
-'use client';
 import { notFound } from 'next/navigation';
 import ProductPage from '@/components/ProductPage';
 import { products } from '@/data/products';
+
+// This function tells Next.js which product IDs to pre-render
+export async function generateStaticParams() {
+  return products.map((product) => ({
+    id: product.id,
+  }));
+}
 
 export default function ProductDetails({ params }: { params: { id: string } }) {
   const product = products.find(p => p.id === params.id);
